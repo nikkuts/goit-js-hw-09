57b20c67d25963252249d4cf638776f796e282da
nikkuts <nickkuts888@gmail.com>
2023-03-09 23:09:58 +0200

    завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const bodyPage = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start=""]');
+const stopBtn = document.querySelector('button[data-stop=""]');
+let timerId = null;
 
+startBtn.addEventListener('click', onChangeColorStart);
+stopBtn.addEventListener('click', onChangeColorStop);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+function onChangeColorStart(event) {
+    timerId = setInterval(() => {
+        bodyPage.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+};
+
+function onChangeColorStop() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+};
