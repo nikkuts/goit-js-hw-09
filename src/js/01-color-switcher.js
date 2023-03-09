e02627310fb19e864cef51fa8b8a4fc0344047e4
nikkuts <nickkuts888@gmail.com>
2023-03-09 23:21:47 +0200

    правки

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,7 +10,7 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   };
 
-function onChangeColorStart(event) {
+function onChangeColorStart() {
     timerId = setInterval(() => {
         bodyPage.style.backgroundColor = getRandomHexColor();
     }, 1000);
