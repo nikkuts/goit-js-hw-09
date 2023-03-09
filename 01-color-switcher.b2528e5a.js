5cb681c26da97c210eddefa64d82c074722b6018
nikkuts <113192352+nikkuts@users.noreply.github.com>
2023-03-09 21:22:11 +0000

    Deploying to gh-pages from @ nikkuts/goit-js-hw-09@e02627310fb19e864cef51fa8b8a4fc0344047e4 🚀

--- 01-color-switcher.b2528e5a.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector('button[data-start=""]'),n=document.querySelector('button[data-stop=""]');let o=null;e.addEventListener("click",(function(n){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.b2528e5a.js.map
