1d35da74ddec01c73dfe3075c35c2ffd26f88db8
nikkuts <113192352+nikkuts@users.noreply.github.com>
2023-03-13 19:55:27 +0000

    Deploying to gh-pages from @ nikkuts/goit-js-hw-09@5a3ab6a9c405192e015e2785c2cb5d054855a6b5 🚀

--- 03-promises.d9aaa88d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("sudmit",(function(e){e.preventDefault();const{elements:{delay:t,step:n,amount:u}}=e.currentTarget;console.log(t.value,n.value,u.value)}));
-//# sourceMappingURL=03-promises.d9aaa88d.js.map
--- 03-promises.ecf27245.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("sudmit",(function(e){e.preventDefault();var t=e.currentTarget.elements,n=t.delay,u=t.step,a=t.amount;console.log(n.value,u.value,a.value)}));
-//# sourceMappingURL=03-promises.ecf27245.js.map
